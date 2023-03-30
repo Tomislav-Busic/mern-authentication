de688040966fa7d7cad6636309373836a575ebd4
Tomislav Busic <tomislav.busic89@gmail.com>
2023-03-30 18:32:58 +0200

    async

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -3,7 +3,7 @@
 @route GET /api/goals
 @access Private
 */
-const getGoals = (req, res) => {
+const getGoals = async (req, res) => {
   res.status(200).json({ message: "Get goals" });
 };
 
@@ -12,7 +12,7 @@ const getGoals = (req, res) => {
 @route POST /api/goals
 @access Private
 */
-const setGoal = (req, res) => {
+const setGoal = async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error("Please add a text field");
@@ -26,7 +26,7 @@ const setGoal = (req, res) => {
 @route PUT /api/goals/:id
 @access Private
 */
-const updateGoal = (req, res) => {
+const updateGoal = async (req, res) => {
   res.status(200).json({ message: `Update goal ${req.params.id}` });
 };
 
@@ -35,7 +35,7 @@ const updateGoal = (req, res) => {
 @route DELETE /api/goals/:id
 @access Private
 */
-const deleteGoal = (req, res) => {
+const deleteGoal = async (req, res) => {
   res.status(200).json({ message: `Delete goal ${req.params.id}` });
 };
 
