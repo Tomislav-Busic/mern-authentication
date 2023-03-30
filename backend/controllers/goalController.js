183a38f8e108e164948139b297da757af608eed3
Tomislav Busic <tomislav.busic89@gmail.com>
2023-03-30 19:25:40 +0200

    setGoal

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -23,7 +23,11 @@ const setGoal = asyncHandler(async (req, res) => {
     throw new Error("Please add a text field");
   }
 
-  res.status(200).json({ message: "New goal" });
+  const goal = await Goal.create({
+    text: req.body.text,
+  });
+
+  res.status(200).json(goal);
 });
 
 /*
