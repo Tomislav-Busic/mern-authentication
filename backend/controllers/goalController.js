252e931a343a91ac1ff10b28c0b4d8141e0acd8b
Tomislav Busic <tomislav.busic89@gmail.com>
2023-03-30 17:50:17 +0200

    setGoal

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -7,6 +7,16 @@ const getGoals = (req, res) => {
   res.status(200).json({ message: "Get goals" });
 };
 
+/*
+@desc Set goal
+@route POST /api/goals
+@access Private
+*/
+const setGoal = (req, res) => {
+  res.status(200).json({ message: "New goal" });
+};
+
 module.exports = {
   getGoals,
+  setGoal
 };
