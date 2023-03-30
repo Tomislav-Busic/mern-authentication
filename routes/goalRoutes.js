3fc75c2fa6c2452fa0ae08a455e8b128ab935479
Tomislav Busic <tomislav.busic89@gmail.com>
2023-03-30 17:28:14 +0200

    router

--- routes/goalRoutes.js
+++ routes/goalRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-app.get("/api/goals", (req, res) => {
+router.get("/", (req, res) => {
   res.status(200).json({ message: "Get goals" });
 });
 
