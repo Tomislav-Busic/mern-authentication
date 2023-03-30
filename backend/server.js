22ecf75364af3a831fbb641a88ce1182f9f1faa8
Tomislav Busic <tomislav.busic89@gmail.com>
2023-03-30 17:26:48 +0200

    use routes

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const port = process.env.PORT;
 
 const app = express();
 
+app.use("/api/goals/", require("./routes/goalRoutes"));
+
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
 /*
