79b6e0765bf32dbbba2b5ca14b730ab233858bec
Tomislav Busic <tomislav.busic89@gmail.com>
2023-04-01 23:33:38 +0200

    initialization

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -16,14 +16,13 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error("Please add all fields");
   }
 
+  // Check if user exists
+  const userExists = await User.findOne({ email });
+
   if (userExists) {
     res.status(400);
     throw new Error("User already exist");
   }
-
-  // Check if user exists
-  const userExists = await User.findOne({ email });
-
   // Hash password
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(password, salt);
