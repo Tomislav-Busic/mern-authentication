8dbe2790c36c7d30c0d0ecb681690ce455db37f3
Tomislav Busic <tomislav.busic89@gmail.com>
2023-04-01 23:15:10 +0200

    public

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -5,7 +5,7 @@ const errorHandler = (err, req, res, next) => {
 
   res.json({
     message: err.message,
-    stack: process.env.NODE_ENV === "development" ? null : err.stack,
+    stack: process.env.NODE_ENV === "public" ? null : err.stack,
   });
 };
 
