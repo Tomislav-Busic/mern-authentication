604842333dd35c05c26f1f5e85ed61ff725f3cdd
Tomislav Busic <tomislav.busic89@gmail.com>
2023-04-01 23:47:08 +0200

    loginUser

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -52,6 +52,23 @@ const registerUser = asyncHandler(async (req, res) => {
 @access Public
 */
 const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+
+    // Check for user email and password
+    const user = await User.findOne({ email });
+    const comparePassword = await bcrypt.compare(password, user.password);
+
+    if (user && comparePassword) {
+      res.json({
+        _id: user.id,
+        name: user.name,
+        email: user.email,
+      });
+    } else {
+      res.status(400);
+      throw new Error("Invalid user data");
+    }
+
   res.json({ message: "Login user" });
 });
 
