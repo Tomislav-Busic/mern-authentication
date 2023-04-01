847bd9b05889258139387c134b001d0fe8f2f5c7
Tomislav Busic <tomislav.busic89@gmail.com>
2023-04-01 22:52:06 +0200

    loginUser

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,19 @@
 /*
 @desc Register new user
-@route POST /api/goals
+@route POST /api/users
 @access Public
 */
 const registerUser = async (req, res) => {
   res.json({ message: "Register user" });
+};
+
+/*
+@desc Authenticate a user
+@route POST /api/users/login
+@access Public
+*/
+const loginUser = async (req, res) => {
+  res.json({ message: "Login user" });
 };
 
 module.exports = { registerUser };
